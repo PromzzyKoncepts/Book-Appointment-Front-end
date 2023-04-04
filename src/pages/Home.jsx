e97505ad60897318e273707db9a5bcf7219ae965
ndikumana isaie <ndikumanaisaie@gmail.com>
2023-04-04 07:21:05 +0200

    Fix log error

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -6,7 +6,7 @@ import { allCars, isLoading } from '../redux/cars/carsSlice';
 
 const Home = () => {
   const cars = useSelector(allCars);
-  console(cars);
+  console.log(cars);
   return (
     <div>Home</div>
   );
