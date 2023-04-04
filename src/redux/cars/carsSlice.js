f58247b7b82c1387454103032a762640fefbc527
ndikumana isaie <ndikumanaisaie@gmail.com>
2023-04-04 07:03:24 +0200

    Implement the carslice reducer

---
+++ src/redux/cars/carsSlice.js
@@ -0,0 +1,34 @@
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
+import { getCars } from '../../api/index';
+
+const FETCH_CARS = 'cars/FETCH_CARS';
+const POST_CAR = 'cars/POST_CAR';
+const DELETE_CAR = 'cars/DELETE_CAR';
+
+export const fetchCars = createAsyncThunk(FETCH_CARS, async () => {
+  const res = await getCars();
+  return res.data;
+});
+
+const initialState = {
+  car: {},
+  allCars: [],
+  loading: 'idle', // 'idle' | 'loading' | 'succeeded' | 'failed'
+  error: null,
+};
+
+const carSlice = createSlice({
+  name: 'cars',
+  initialState,
+  reducers: {},
+  extraReducers(builder) {
+    builder
+      .addCase(fetchCars.pending, (state) => ({ ...state, loading: 'Loading' }))
+      .addCase(fetchCars.fulfilled, (state, action) => ({ ...state, loading: 'succeeded', allCars: action.payload }))
+      .addCase(fetchCars.rejected, (state) => ({ ...state, loading: 'Failed' }));
+  },
+});
+
+export const isLoading = (state) => state.cars.loading;
+export const allCars = (state) => state.cars.allCars;
+export default carSlice.reducer;
