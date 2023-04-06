e06db521a7e0e25b0efb52228e7392a6a0237a54
PromzzyKoncepts <okechukwupromise638@gmail.com>
2023-04-06 10:05:38 +0100

    Add routes

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
+import { Route, Routes } from 'react-router-dom';
 import Home from './pages/Home';
+import Login from './components/auth/Login';
+import Register from './components/auth/Register';
 
 function App() {
   return (
     <div>
-      <Home />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+      </Routes>
     </div>
   );
 }
