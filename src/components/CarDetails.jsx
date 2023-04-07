9b4984694a673b6063e1433bd84b5c42993e493f
ndikumanaisaie <ndikumanaisaie@gmail.com>
2023-04-07 07:40:34 +0200

    Restructure Cardetails markup and styles

--- src/components/CarDetails.jsx
+++ src/components/CarDetails.jsx
@@ -17,35 +17,43 @@ const CarDetails = () => {
       <div className="car_container" key={cardetails.id}>
         <img src={cardetails.image_url} alt={cardetails.name} />
         <div className="car_details">
-          <h2>{cardetails.name}</h2>
-          <ul>
-            <li>
-              Model:
-              {' '}
-              <span>{cardetails.model}</span>
-            </li>
-            <li className="price">
-              Price:
-              {' '}
-              <span>
-                {cardetails.price}
-                $
-              </span>
-            </li>
-            <li>
-              Availability:
+          <div className="car-header">
+            <h2>{cardetails.name}</h2>
+            <p>{cardetails.description}</p>
+          </div>
+          <div className="finance fee">
+            <p>Finance Fee</p>
+            <p>$139</p>
+          </div>
+          <div className="option-fee">
+            <p>Option to purchase Fee</p>
+            <p>$249</p>
+          </div>
+          <div className="total-amount fee">
+            <p>Total amount payable</p>
+            <p>{cardetails.price}</p>
+          </div>
+          <div className="duration">
+            <p>Duration</p>
+            <p>48 Months</p>
+          </div>
+          <div className="availability">
+            <p>Availability:</p>
+            <p>
               {cardetails.reserved === false ? (
                 <span>Not available</span>
               ) : (
                 <span>available</span>
               )}
-            </li>
-          </ul>
-          <span className="desc_short">We can satisfy your desire!</span>
+            </p>
+          </div>
+          <div className="more-model">
+            <span className="desc_short">We can satisfy your desire!</span>
+          </div>
           <Link to="/reservations">
             <button
               type="button"
-              className="add-button"
+              className="reserve"
             >
               Reserve
             </button>
