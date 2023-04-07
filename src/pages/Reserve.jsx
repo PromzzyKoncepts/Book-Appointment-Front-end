48acfa05f40af237f9e81305cf4308bdcc3ae604
Abdullah Asghar <dulakhan024@gmail.com>
2023-04-07 12:27:34 +0200

    Create reserve page

--- src/pages/Reserve.jsx
+++ src/pages/Reserve.jsx
@@ -3,3 +3,9 @@ import React, { useState } from 'react';
 const ReservePage = ({ username, selectedItem }) => {
   const [date, setDate] = useState('');
   const [city, setCity] = useState('');
+  
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    // Here you can send the reservation data to your backend or do any other necessary logic
+    console.log(`Reservation for ${selectedItem} on ${date} in ${city} made by ${username}`);
+  };
