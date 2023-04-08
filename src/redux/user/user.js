1bed4449a8b45c1d44632b731f3a6c4fca416b0a
PromzzyKoncepts <okechukwupromise638@gmail.com>
2023-04-08 10:06:25 +0100

    impplement loggedin states

--- src/redux/user/user.js
+++ src/redux/user/user.js
@@ -4,8 +4,9 @@ const userSlice = createSlice({
   name: 'user',
   initialState: {
     currentUser: null,
-    isFetching: true,
+    isFetching: false,
     error: false,
+    isLoggedIn: !!localStorage.getItem('token'),
   },
   reducers: {
     loginStart: (state) => {
@@ -14,13 +15,24 @@ const userSlice = createSlice({
     loginSuccess: (state, action) => {
       state.isFetching = false;
       state.currentUser = action.payload;
+      state.isLoggedIn = true;
+      localStorage.setItem('user', JSON.stringify(action.payload));
     },
     loginFailure: (state) => {
       state.isFetching = false;
       state.error = true;
+      state.isLoggedIn = false;
+    },
+    logout: (state) => {
+      localStorage.removeItem('token');
+      localStorage.removeItem('user');
+      state.currentUser = null;
+      state.isLoggedIn = false;
     },
   },
 });
 
-export const { loginStart, loginSuccess, loginFailure } = userSlice.actions;
+export const {
+  loginStart, loginSuccess, loginFailure, logout,
+} = userSlice.actions;
 export default userSlice.reducer;
