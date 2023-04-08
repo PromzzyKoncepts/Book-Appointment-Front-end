60f45f9e89883c2acb5cfbc187fbefb02b6374d6
PromzzyKoncepts <okechukwupromise638@gmail.com>
2023-04-08 10:05:46 +0100

    remove console

--- src/redux/cars/carsSlice.js
+++ src/redux/cars/carsSlice.js
@@ -11,7 +11,6 @@ const FETCH_CAR = 'cars/FETCH_CAR';
 export const fetchCars = createAsyncThunk(FETCH_CARS, async () => {
   try {
     const response = await getCars();
-    console.log(response.data);
     return response.data;
   } catch (error) {
     return error.response.data;
@@ -21,7 +20,6 @@ export const fetchCars = createAsyncThunk(FETCH_CARS, async () => {
 export const fetchCar = createAsyncThunk(FETCH_CAR, async (id) => {
   try {
     const response = await getCar(id);
-    console.log(response.data);
     return response.data;
   } catch (error) {
     return error.response.data;
@@ -36,7 +34,6 @@ export const createCar = createAsyncThunk(CREATE_CAR, async (
     const response = await addCar(updatedCarData);
     toast.success('Car created successfully!');
     navigate('/');
-    console.log(response.data);
     return response.data;
   } catch (error) {
     return rejectWithValue(error.response.data);
@@ -50,7 +47,6 @@ export const removeCar = createAsyncThunk(DELETE_CAR, async (
     const response = await deteleCar(id);
     toast.success('Car deleted successfully!');
     // navigate('/');
-    console.log(response.data);
     return response.data;
   } catch (error) {
     return rejectWithValue(error.response.data);
@@ -87,7 +83,7 @@ const carSlice = createSlice({
       .addCase(createCar.pending, (state) => ({ ...state, loading: 'Loading' }))
       .addCase(createCar.fulfilled, (state, action) => ({
         ...state,
-        allCars: action.payload.data,
+        allCars: [...state.allCars, action.payload.data],
         message: action.payload.message,
         loading: action.payload.status,
       }))
