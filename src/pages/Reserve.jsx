aadeda1b11347aad0c7ce65186c69539c1365071
Abdullah Asghar <dulakhan024@gmail.com>
2023-04-09 11:49:15 +0200

    Fix Reserve.jsx

--- src/pages/Reserve.jsx
+++ src/pages/Reserve.jsx
@@ -1,49 +1,89 @@
+/* eslint-disable react/jsx-props-no-spreading */
+/* eslint-disable camelcase */
 import React, { useState } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { useNavigate } from 'react-router-dom';
+import DatePicker from 'react-date-picker';
+import 'react-date-picker/dist/DatePicker.css';
+import { toast } from 'react-toastify';
+import { allCars } from '../redux/cars/carsSlice';
+import { createReservation } from '../redux/reservations/reservationsSlice';
+import {
+  Form, FormBtn, FormDiv, H1, Input,
+} from '../Styles';
 
-const Reserve = ({ username, selected }) => {
-  const [date, setDate] = useState('');
-  const [city, setCity] = useState('');
+const Reserve = () => {
+  const cars = useSelector(allCars);
 
-  const handleDateChange = (event) => {
-    setDate(event.target.value);
-  };
+  const currentUserData = JSON.parse(localStorage.getItem('user'));
+  const currentUser = currentUserData.user;
 
-  const handleCityChange = (event) => {
-    setCity(event.target.value);
+  const [pickupDate, setPickupDate] = useState(new Date());
+  const [returnDate, setReturnDate] = useState(new Date());
+  const [carId, setCarId] = useState(1);
+
+  const [reservationData, setreservationData] = useState({
+    pickup_date: pickupDate,
+    return_date: returnDate,
+    city: '',
+    user_id: currentUser.id,
+    car_id: carId,
+  });
+
+  console.log(carId);
+
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+
+  const { city } = reservationData;
+
+  const handleClear = () => {
+    setreservationData({
+      ...reservationData,
+      pickup_date: '',
+      return_date: '',
+      city: '',
+    });
   };
 
   const handleSubmit = (event) => {
     event.preventDefault();
-    // Here you can write the logic to save the reservation details to a database or API
+
+    dispatch(createReservation({ reservationData, navigate, toast }));
+    handleClear();
+  };
+
+  const onInputChange = (e) => {
+    const { name, value } = e.target;
+    setreservationData({ ...reservationData, [name]: value });
+  };
+
+  const handleCarIdChange = (e) => {
+    setCarId(e.target.value);
   };
 
   return (
-    <div className="reserve-container">
-      <h2>Reserve</h2>
-      <form onSubmit={handleSubmit}>
-        <label>
-          Username:
-          <input type="text" value={username} readOnly />
-        </label>
-        <br />
-        <label>
-          Item:
-          <input type="text" value={selected} readOnly />
-        </label>
-        <br />
-        <label>
-          Date:
-          <input type="date" value={date} onChange={handleDateChange} required />
-        </label>
-        <br />
-        <label>
-          City:
-          <input type="text" value={city} onChange={handleCityChange} required />
-        </label>
-        <br />
-        <button type="submit">Reserve Now</button>
-      </form>
-    </div>
+    <FormDiv>
+      <H1>Reserve</H1>
+      <Form onSubmit={handleSubmit}>
+        <Input type="text" name={currentUser.name} placeholder="user" aria-label="name" value={currentUser.name} readOnly />
+        <select
+          aria-label="Select label"
+          value={carId}
+          onChange={handleCarIdChange}
+        >
+          { Array.isArray(cars) && cars?.map((car) => (
+            <option key={car.id} value={car.id}>
+              {car.name}
+            </option>
+          ))}
+        </select>
+        <DatePicker onChange={setPickupDate} value={pickupDate} />
+        <DatePicker onChange={setReturnDate} value={returnDate} />
+        <Input type="text" aria-label="city" name="city" value={city} onChange={onInputChange} required />
+        <FormBtn type="submit">Reserve Now</FormBtn>
+      </Form>
+    </FormDiv>
   );
 };
 
