d1dfda2310d7b30e099dd9cf784f924bceb7c0b6
Abdullah Asghar <dulakhan024@gmail.com>
2023-04-09 11:58:45 +0200

    Styles add

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.scss';
+import './styles.css';
 import { Provider } from 'react-redux';
 import { BrowserRouter } from 'react-router-dom';
 import store from './redux/configureStore';
