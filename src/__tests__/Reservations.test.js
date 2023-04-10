fedfe90fbce769db737b1d7fb37bd59dacf6f960
Abdullah Asghar <dulakhan024@gmail.com>
2023-04-10 12:46:26 +0200

    Testing error

--- src/__tests__/Reservations.test.js
+++ src/__tests__/Reservations.test.js
@@ -1,21 +1,28 @@
 import React from 'react';
+import '@testing-library/jest-dom';
+import TestRenderer from 'react-test-renderer';
 import { Provider } from 'react-redux';
-import { render, screen } from '@testing-library/react';
-import Reservations from '../pages/Reservations';
+import MyReservations from '../pages/Reservations';
 import store from '../redux/configureStore';
 
-describe('Reservations component', () => {
-  test('renders the component', async () => {
-    render(
+describe('MyReservations', () => {
+  it('renders correctly', () => {
+    const tree = TestRenderer.create(
       <Provider store={store}>
-        <Reservations />
-      </Provider>
+        <MyReservations />
+      </Provider>,
+    ).toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+
+  it('display MY RESERVATIONS', () => {
+    const testRenderer = TestRenderer.create(
+      <Provider store={store}>
+        <MyReservations />
+      </Provider>,
     );
-    const heading = screen.getByRole('heading', { level: 2 });
-    expect(heading.textContent).toBe('My Reservations');
-    const tableHeaders = screen.getAllByRole('columnheader');
-    expect(tableHeaders.length).toBe(3);
-    const rows = screen.getAllByRole('row');
-    expect(rows.length).toBeGreaterThan(1);
+    const testInstance = testRenderer.root;
+    const h1 = testInstance.findByType('h1');
+    expect(h1.children).toEqual(['My Reservations']);
   });
 });
--- src/__tests__/Reserve.test.js
+++ src/__tests__/Reserve.test.js
@@ -6,3 +6,32 @@ import Reserve from '../pages/Reserve';
 import store from '../redux/store';
 
 
+
+describe('Reserve', () => {
+  it('should submit reservation data when form is submitted', async () => {
+    const { getByLabelText, getByText } = render(
+      <Provider store={store}>
+        <MemoryRouter>
+          <Reserve />
+        </MemoryRouter>
+      </Provider>,
+    );
+
+    // Select a car from the dropdown
+    const carDropdown = getByLabelText('Select label');
+    fireEvent.change(carDropdown, { target: { value: '2' } });
+
+    // Fill in the city input
+    const cityInput = getByLabelText('city');
+    fireEvent.change(cityInput, { target: { value: 'New York' } });
+
+    // Submit the form
+    const submitBtn = getByText('Reserve Now');
+    fireEvent.click(submitBtn);
+
+    // Wait for the reservation to be created
+    await waitFor(() => {
+      expect(store.getState().reservations.reservations.length).toBe(1);
+    });
+  });
+});
