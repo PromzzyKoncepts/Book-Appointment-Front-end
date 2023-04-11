9c130ecdb11964d1985dc8450b94bf33f71099dd
Abdullah Asghar <dulakhan024@gmail.com>
2023-04-11 13:16:50 +0200

    Fix some errors

--- src/setupTests.js
+++ src/setupTests.js
@@ -1,20 +1,19 @@
-var polyfill = require("polyfill");
+const polyfill = require('polyfill');
 require('jest-localstorage-mock');
 
-global.matchMedia =
-  global.matchMedia ||
-  function () {
+global.matchMedia = global.matchMedia
+  || function () {
     return {
       matches: false,
-      addListener: function () {},
-      removeListener: function () {},
+      addListener() {},
+      removeListener() {},
     };
   };
 
-  const localStorageMock = {
-    getItem: jest.fn(),
-    setItem: jest.fn(),
-    removeItem: jest.fn(),
-    clear: jest.fn(),
-  };
-  global.localStorage = localStorageMock;
+const localStorageMock = {
+  getItem: jest.fn(),
+  setItem: jest.fn(),
+  removeItem: jest.fn(),
+  clear: jest.fn(),
+};
+global.localStorage = localStorageMock;
